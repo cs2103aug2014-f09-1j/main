f2d7b98314e484767e5a938e6db3f17c3c6b344b
Zhan <Zhan@172.28.187.16>
2014-09-25 16:56:10 +0800

    Added add-task function.

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -1,64 +1,120 @@
-package Logic;
-
-import Structure.Operation.OPCODE;
-
-import java.util.ArrayList;
-
 /*
  * This is the Logic class.
  */
 
+package Logic;
+
+import Structure.Task;
+import Structure.Operation.OPCODE;
+import java.util.ArrayList;
+
 public class Logic {
 	
-	public static ArrayList<String> list = new ArrayList<String>();
+	public static ArrayList<Task> list = new ArrayList<Task>();
 	public static ArrayList<String> output = new ArrayList<String>();
 	
 	public static String MESSAGE_ADDED = "Task successfully added";
 	public static String MESSAGE_DELETED = "Task successfully deleted";
 	public static String MESSAGE_UPDATED = "Task successfully updated";
 	
+	static int numberOfTasks = 0;
 	
-	public static void main(String[] args) {
-		 
-
-	}
-	
-	public static void execute(OPCODE command) {		 
+	public static void execute(OPCODE command, Task task) {
+		Task temp = (Task) task;
+		
 		switch (command) {
 		case ADD:
-			addTask();
+			addTask(temp);
 			break;
 		case DELETE:
-			deleteTask();
+			deleteTask(temp);
+			break;
+		case UPDATE:
+			updateTask(temp);
 			break;
 		case VIEW:
 			viewTask();
 			break;
-		case UPDATE:
-			updateTask();
-			break;
+		default:
+			break;		
 		}
 	}
 	
-	public static void addTask() {
+	public static String addTask(Task task) {
+		int task_ID = numberOfTasks + 1;
 		
+		list.add(task_ID, task);
+		
+		return MESSAGE_ADDED;
 	}
+	
+	public static String deleteTask(Task temp) {
+		String deleteType = temp.getDeleteType();
+		
+		switch (deleteType) {
+		case "ID":
+			break;
+		case "DEADLINE":
+			break;
+		case "DATE":
+			break;
+		case "TIMEFRAME":
+			break;
+		default:
+			break;	
+		}
+		
+		return MESSAGE_DELETED;
+	}	
+	
+	public static String updateTask(Task task) {
+		
+		return MESSAGE_UPDATED;
+	}	
 	
 	public static void viewTask() {
 		
 	}
 	
-	public static void updateTask() {
-		
-	}
 	
-	public static void deleteTask() {
-		
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void storeIntoList() {
+
+		for (int i = 0; i < numberOfTasks; i++) {
+			Task temp = new Task();
+			list.add(i+1, temp);
+			/*
+			 * Reading in every element and creating the list.
+			 */
+		}
+	}	
 	
 	public static void getOutput() {
 		
 	}
+	
+	
+	public static void deleteById(int id) {
+		list.remove(id);
+	}
+	
+	public static void deleteByDeadline(int time) {
+	}
+	
+	
+	public static void viewNext(Task task) {
+	}
+		
+	public static void viewAll() {		
+	}	
 }
 
 

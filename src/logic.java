1df7f2c14b46b641445e2421bee511b95a5123c0
Zhan <Zhan@172.28.187.16>
2014-09-25 11:48:15 +0800

    Initial commit for Logic

---
+++ src/logic.java
@@ -0,0 +1,47 @@
+
+
+
+
+import java.util.ArrayList;
+
+/*
+ * This is the Logic class.
+ */
+
+public class logic {
+	
+	public static ArrayList<String> list = new ArrayList<String>();
+	
+	public static String ADDED_Message = "Task successfully added";
+	public static String DELETED_Message = "Task successfully added";
+	public static String UPDATED_Message = "Task successfully added";
+	public static String SEARCH_Message = "Task successfully added";
+	public static String LABELED_Message Task successfully added;
+	public static String UNDO_Message Task successfully added;
+	
+	
+	public static void main(String[] args) {
+		String 
+
+	}
+	
+	public static void execute() {
+		
+	}
+	
+	public static String addTask() {
+		return 
+	}
+	
+	public static String viewTask() {
+		
+	}
+	
+	public static String updateTask() {
+		
+	}
+	
+	public static String deleteTask() {
+		
+	}
+}

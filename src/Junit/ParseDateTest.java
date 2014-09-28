73cebc2bb47e4ec2f539f90a000c3da9d16cb6ca
kohjx <junxiang1991@gmail.com>
2014-09-28 20:02:10 +0800

    Add a ParseDate class to handle parsing of a date input

---
+++ src/Junit/ParseDateTest.java
@@ -0,0 +1,56 @@
+package Junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Parser.ParseDate;
+
+public class ParseDateTest {
+
+	@Test
+	public void testParseDate() {
+		ParseDate parseDate = new ParseDate();
+		String formattedDate = "";
+		formattedDate = parseDate.parseInput("2359 010414");
+		assertEquals("Test HHmm ddMMyy", "201404012359", formattedDate);
+		
+		formattedDate = parseDate.parseInput("0102 04012014");
+		assertEquals("Test HHmm ddMMyyyy", "201401040102", formattedDate);
+		
+		formattedDate = parseDate.parseInput("0000 04012014");
+		assertEquals("Test time 0000 ", "201401040000", formattedDate);
+		
+		formattedDate = parseDate.parseInput("010414 2359");
+		assertEquals("Test ddMMyy HHmm", "201404012359", formattedDate);
+		
+		formattedDate = parseDate.parseInput("04012014 0102");
+		assertEquals("Test ddMMyyyy HHmm", "201401040102", formattedDate);
+		
+		formattedDate = parseDate.parseInput("1315 25/12/14");
+		assertEquals("Test HHmm dd/MM/yy", "201412251315", formattedDate);
+		
+		formattedDate = parseDate.parseInput("1315 25/12/2014");
+		assertEquals("Test HHmm dd/MM/yyyy", "201412251315", formattedDate);
+		
+		formattedDate = parseDate.parseInput("25/12/14 1315");
+		assertEquals("Test dd/MM/yy HHmm", "201412251315", formattedDate);
+		
+		formattedDate = parseDate.parseInput("25/12/2014 1315");
+		assertEquals("Test dd/MM/yyyy HHmm", "201412251315", formattedDate);
+		
+		formattedDate = parseDate.parseInput("0909 03-04-14");
+		assertEquals("Test HHmm dd-MM-yy", "201404030909", formattedDate);
+		
+		formattedDate = parseDate.parseInput("0909 03-04-2014");
+		assertEquals("Test HHmm dd-MM-yyyy", "201404030909", formattedDate);
+		
+		formattedDate = parseDate.parseInput("03-04-14 0909");
+		assertEquals("Test dd-MM-yy HHmm", "201404030909", formattedDate);
+		
+		formattedDate = parseDate.parseInput("03-04-2014 0909");
+		assertEquals("Test dd-MM-yyyy HHmm", "201404030909", formattedDate);
+		
+	}
+
+}
---
+++ src/Parser/ParseDate.java
@@ -0,0 +1,89 @@
+package Parser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Vector;
+
+public class ParseDate {
+	
+	private final String[] FORMATS_TIME = {"HHmm"};
+	private final String[] FORMATS_DATE = {"ddMMyy", "ddMMyyyy", "dd/MM/yy", "dd/MM/yyyy", "dd-MM-yy", "dd-MM-yyyy"};
+	
+	public String parseInput(String input){
+		Vector<String> allFormats = getAllFormats();
+		String formattedDate = getFormattedDate(input, allFormats);
+		return formattedDate;
+	}
+	
+	private Vector<String> getAllFormats(){
+		Vector<String> allFormats = new Vector<String>();
+		for(String time : FORMATS_TIME ){
+			for (String date : FORMATS_DATE){
+				allFormats.add(time + " " + date);
+			}
+		}
+		for(String date : FORMATS_DATE ){
+			for (String time : FORMATS_TIME){
+				allFormats.add(date + " " + time);
+			}
+		}
+		
+		return allFormats;
+	}
+	
+	private String getFormattedDate(String input, Vector<String> allFormats){
+		String formattedDate = "";
+		
+		for(String format : allFormats){
+			try{
+				SimpleDateFormat formatter = new SimpleDateFormat(format);
+				formatter.setLenient(false);
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(formatter.parse(input));
+				formattedDate = formatDate(cal);
+				break;
+			} catch (ParseException e){
+				//Do nothing. Continue with other formats
+			}
+		}
+		
+		return formattedDate;
+	}
+
+	private String formatDate(Calendar cal) {
+		
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH)+1;
+		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+		String twoDigitMonth = "";
+		String twoDigitDayOfMonth = "";
+		String twoDigitHour = "";
+		String twoDigitMinute = "";
+		
+		if(month < 10){
+			twoDigitMonth = "0" + month;
+		} else {
+			twoDigitMonth = "" + month;
+		}
+		if(dayOfMonth < 10){
+			twoDigitDayOfMonth = "0" + dayOfMonth;
+		} else {
+			twoDigitDayOfMonth = "" + dayOfMonth;
+		}
+		if (hour < 10){
+			twoDigitHour = "0" + hour;
+		} else {
+			twoDigitHour = "" + hour;
+		}
+		if (minute < 10){
+			twoDigitMinute = "0" + minute;
+		} else {
+			twoDigitMinute = "" + minute;
+		}
+		
+		return year + twoDigitMonth + twoDigitDayOfMonth + twoDigitHour + twoDigitMinute; 
+	}
+}

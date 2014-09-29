b432443343f34135932946be6e27da772083673c
danqi <A0092165@gmail.com>
2014-09-29 10:12:30 +0800

    added 2 test cases: add and update in ParserTest to test the interaction with Parser andExtractor.

--- src/Junit/ParserTest.java
+++ src/Junit/ParserTest.java
@@ -1,11 +1,13 @@
 package Junit;
 import static org.junit.Assert.*;
+import Structure.OPCODE;
 
 import org.junit.Test;
 
 import java.util.Scanner;
 
 import Parser.*;
+import Structure.Task;
 public class ParserTest {
 
 	private Scanner scanner = new Scanner(System.in);
@@ -24,5 +26,34 @@ public class ParserTest {
 		assertEquals(description, expected, output);
 		
 	}
+	
+	@Test
+	public void testParserAdd() {
+		description = "Parser testing: Add command";
+		input = "add dine with Amy from 201409291800 to 201409292000";	
+		Parser parser = new Parser(input);
+		parser.parseInput();
+        Task output = parser.getTask();
+		assertEquals("Test Add 1 - description", "dine with Amy", output.getDescription());
+		assertEquals("Test Add 1 - startTime", "201409291800", output.getStartTime());
+		assertEquals("Test Add 1 - endTime", "201409292000", output.getEndTime());
+        assertEquals("Test Add 1 - OPCODE",OPCODE.ADD,output.getOpCode());
+	}
+	
+	@Test
+	public void testParserUpdate() {
+		description = "Parser testing: Add command";
+		input = "update 19 from 201409291800 to 201409292000";
+		Parser parser = new Parser(input);
+		parser.parseInput();
+        Task output = parser.getTask();
+		assertEquals("Test update 1 - description", null, output.getDescription());
+		assertEquals("Test update 1 - startTime", "201409291800", output.getStartTime());
+		assertEquals("Test update 1 - endTime", "201409292000", output.getEndTime());		
+        assertEquals("Test update 1 - OPCODE",OPCODE.UPDATE,output.getOpCode());
+        assertEquals("Test update 1 - taskID",19,parser.getTaskID());
+        assertEquals("Test update 1 - updateType","TIMEFRAME",output.getUpdateType());
+	}
+	
 
 }

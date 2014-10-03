53ff8b64462901a32375ca8cc477dd0b2681a2cb
anand-sundaram <anand.sundaram@nus.edu.sg>
2014-10-03 13:03:08 +0530

    Changes made to the test cases
    Reflects the changes made to the delimiter in Storage

--- src/Junit/StorageTest.java
+++ src/Junit/StorageTest.java
@@ -15,7 +15,7 @@ public class StorageTest {
 	@Test
 	public void testStringToTask() {	
 		Storage obj = new Storage();
-		Task dummyTask1 = obj.StringToTask("DummyDescription DummyStartTime DummyEndTime");
+		Task dummyTask1 = obj.StringToTask("DummyDescription,DummyStartTime,DummyEndTime");
 		assertEquals(dummyTask1.getDescription(), "DummyDescription");
 		assertEquals(dummyTask1.getStartTime(), "DummyStartTime");
 		assertEquals(dummyTask1.getEndTime(), "DummyEndTime");		
@@ -24,9 +24,9 @@ public class StorageTest {
 	@Test
 	public void testInputAndReadTasks() throws IOException {
 		Storage obj = new Storage();
-		Task dummyTask1 = obj.StringToTask("DummyDescription1 DummyStartTime1 DummyEndTime1");
-		Task dummyTask2 = obj.StringToTask("DummyDescription2 DummyStartTime2 DummyEndTime2");
-		Task dummyTask3 = obj.StringToTask("DummyDescription3 DummyStartTime3 DummyEndTime3");
+		Task dummyTask1 = obj.StringToTask("DummyDescription1,DummyStartTime1,DummyEndTime1");
+		Task dummyTask2 = obj.StringToTask("DummyDescription2,DummyStartTime2,DummyEndTime2");
+		Task dummyTask3 = obj.StringToTask("DummyDescription3,DummyStartTime3,DummyEndTime3");
 		
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		tasks.add(dummyTask1);

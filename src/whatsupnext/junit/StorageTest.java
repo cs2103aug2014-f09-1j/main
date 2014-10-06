77177bbafc9e57119cabf08142e20d36b677f351
anand-sundaram <anand.sundaram@nus.edu.sg>
2014-10-06 11:21:33 +0530

    Made changes to test cases to enable use of taskID

--- src/whatsupnext/junit/StorageTest.java
+++ src/whatsupnext/junit/StorageTest.java
@@ -15,7 +15,8 @@ public class StorageTest {
 	@Test
 	public void testStringToTask() {	
 		Storage obj = new Storage();
-		Task dummyTask1 = obj.StringToTask("DummyDescription,DummyStartTime,DummyEndTime");
+		Task dummyTask1 = obj.StringToTask("DummyTaskID,DummyDescription,DummyStartTime,DummyEndTime");
+		assertEquals(dummyTask1.getTaskID(), "DummyTaskID");
 		assertEquals(dummyTask1.getDescription(), "DummyDescription");
 		assertEquals(dummyTask1.getStartTime(), "DummyStartTime");
 		assertEquals(dummyTask1.getEndTime(), "DummyEndTime");		
@@ -24,9 +25,11 @@ public class StorageTest {
 	@Test
 	public void testInputAndReadTasks() throws IOException {
 		Storage obj = new Storage();
-		Task dummyTask1 = obj.StringToTask("DummyDescription1,DummyStartTime1,DummyEndTime1");
-		Task dummyTask2 = obj.StringToTask("DummyDescription2,DummyStartTime2,DummyEndTime2");
-		Task dummyTask3 = obj.StringToTask("DummyDescription3,DummyStartTime3,DummyEndTime3");
+		obj.clearFile();
+		
+		Task dummyTask1 = obj.StringToTask("DummyTaskID1,DummyDescription1,DummyStartTime1,DummyEndTime1");
+		Task dummyTask2 = obj.StringToTask("DummyTaskID2,DummyDescription2,DummyStartTime2,DummyEndTime2");
+		Task dummyTask3 = obj.StringToTask("DummyTaskID3,DummyDescription3,DummyStartTime3,DummyEndTime3");
 		
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		tasks.add(dummyTask1);
@@ -43,7 +46,7 @@ public class StorageTest {
 	}
 	
 	public String taskToString(Task t) {
-		return (t.getDescription() + "," + t.getStartTime() + "," + t.getEndTime());
+		return (t.getTaskID() + "," + t.getDescription() + "," + t.getStartTime() + "," + t.getEndTime());
 	}
 
 }

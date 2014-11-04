d670d859a7d9890e20e8085573fe532a9aa4f8b5
kohjx <junxiang1991@gmail.com>
2014-11-04 22:29:53 +0800

    Struture: updated format of help message in Help to suit with latest GUI

--- src/whatsupnext/structure/Help.java
+++ src/whatsupnext/structure/Help.java
@@ -7,58 +7,67 @@ public class Help {
 	
 	public static final String DEFAULT_HELP_MESSAGE = "Type \"help <command>\" to find out more."
 			+ NEWLINE + "Supported Commands:"
-			+ NEWLINE + "add" + INDENT + "Add a task by with description only or"
-			+ NEWLINE + INDENT + "append it with a deadline or time period."
-			+ NEWLINE + "view" + INDENT + "Display the upcoming task or the tasks"
-			+ NEWLINE + INDENT + "for the day. A time frame can be" 
-			+ NEWLINE + INDENT + "specified too."
-			+ NEWLINE + "update" + INDENT + "Update the description, deadline, start"
-			+ NEWLINE + INDENT + "or end time of a task."
-			+ NEWLINE + "delete" + INDENT + "Delete by id, date, deadline, or time"
-			+ NEWLINE + INDENT + "frame."
+			+ NEWLINE + "add" + INDENT + "Add a task by with description only"
+			+ NEWLINE + INDENT + "or append it with a deadline or time"
+			+ NEWLINE + INDENT + "period."
+			+ NEWLINE + "view" + INDENT + "Display the upcoming task or the"
+			+ NEWLINE + INDENT + "tasks for the day. A time frame can" 
+			+ NEWLINE + INDENT + "be specified too."
+			+ NEWLINE + "update" + INDENT + "Update the description, deadline,"
+			+ NEWLINE + INDENT + "start or end time of a task."
+			+ NEWLINE + "delete" + INDENT + "Delete by id, date, deadline, or"
+			+ NEWLINE + INDENT + "time frame."
 			+ NEWLINE + "search" + INDENT + "Search for a task by its keyword."
 			+ NEWLINE + "done" + INDENT + "Label a task as done."
 			+ NEWLINE + "undo" + INDENT + "Undo most recent add/update/delete."
 			+ NEWLINE + "redo" + INDENT + "Redo the most recent undo."
 			+ NEWLINE + "exit" + INDENT + "Close WhatsUpNext.";
 	
-	public static final String ADD_HELP_MESSAGE = "add" + INDENT + "Add a task by with description only or"
-			+ NEWLINE + INDENT + "append it with a deadline or time period."
+	public static final String ADD_HELP_MESSAGE = "add" + INDENT + "Add a task by with description only"
+			+ NEWLINE + INDENT + "or append it with a deadline or time"
+			+ NEWLINE + INDENT + "period."
+			+ NEWLINE
 			+ NEWLINE + "Formats supported:"
 			+ NEWLINE + "add [task]"
 			+ NEWLINE + "add [task] by [end_time]" 
 			+ NEWLINE + "add [task] from [start_time] to [end_time]";
 	
-	public static final String VIEW_HELP_MESSAGE = "view" + INDENT + "Display the upcoming task or the tasks"
-			+ NEWLINE + INDENT + "for the day. A time frame can be" 
-			+ NEWLINE + INDENT + "specified too."
+	public static final String VIEW_HELP_MESSAGE = "view" + INDENT + "Display the upcoming task or the"
+			+ NEWLINE + INDENT + "tasks for the day. A time frame can" 
+			+ NEWLINE + INDENT + "be specified too."
+			+ NEWLINE
 			+ NEWLINE + "Formats supported:"
 			+ NEWLINE + "view all"
 			+ NEWLINE + "view next"
 			+ NEWLINE + "view [day|date]"
 			+ NEWLINE + "view from [start_time] to [end_time]";
 	
-	public static final String UPDATE_HELP_MESSAGE =  "update" + INDENT + "Update the description, deadline, start"
-			+ NEWLINE + INDENT + "or end time of a task."
+	public static final String UPDATE_HELP_MESSAGE =  "update" + INDENT + "Update the description, deadline,"
+			+ NEWLINE + INDENT + "start or end time of a task."
+			+ NEWLINE
 			+ NEWLINE + "Formats supported:"
 			+ NEWLINE + "update [task_id] [new_description]"
 			+ NEWLINE + "update [task_id] by [end_time]"
 			+ NEWLINE + "update [task_id] from [start_time] to [end_time]";
 	
-	public static final String DELETE_HELP_MESSAGE = "Delete by id, date, deadline, or time frame."
+	public static final String DELETE_HELP_MESSAGE = "delete" + INDENT + "Delete by id, date, deadline, or"
+			+ NEWLINE + INDENT + "time frame."
+			+ NEWLINE
 			+ NEWLINE + "Formats supported:"
 			+ NEWLINE + "delete [task_id]"
 			+ NEWLINE + "delete deadline"
 			+ NEWLINE + "delete [date]"
 			+ NEWLINE + "delete from [start_time] to [end_time]";
 	
-	public static final String DONE_HELP_MESSAGE = "Label a task as done."
+	public static final String DONE_HELP_MESSAGE = "done" + INDENT + "Label a task as done."
+			+ NEWLINE
 			+ NEWLINE + "Formats supported:"
 			+ NEWLINE + "done [task_id]";
 	
 	public static final String SEARCH_HELP_MESSAGE = "search" + INDENT + "Search for a task by its keyword."
-	+ NEWLINE + "Formats supported:" 
-	+ NEWLINE + "search [keywords]";
+			+ NEWLINE
+			+ NEWLINE + "Formats supported:" 
+			+ NEWLINE + "search [keywords]";
 	
 	public static final String UNDO_HELP_MESSAGE = "undo" + INDENT + "Undo most recent add/update/delete.";
 	

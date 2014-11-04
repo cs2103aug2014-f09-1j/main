06c5a11479bf902cb6e9e3a27fafd48276e21da0
kohjx <junxiang1991@gmail.com>
2014-11-04 17:28:13 +0800

    Structure: Updated verbose help message in Help

--- src/whatsupnext/structure/Help.java
+++ src/whatsupnext/structure/Help.java
@@ -25,54 +25,57 @@ public class Help {
 	public static final String ADD_HELP_MESSAGE_BRIEF = "Add a task by specifying the task description only or" + "\n" 
 			+ " a specific deadline or a time period."; 
 	
-	public static final String ADD_HELP_MESSAGE_DETAILED = "Add a task by specifying the task description only or" + "\n"
-			+ " a specific deadline or a time period." + "\n"
-			+ "Formats supported:" + "\n"
-			+ "	add [task]" + "\n"
-			+ "	add [task] by [end_time]" + "\n" 
-			+ "	add [task] from [start_time] to [end_time]";
+	public static final String ADD_HELP_MESSAGE_DETAILED = "add" + INDENT + "Add a task by with description only or"
+			+ NEWLINE + INDENT + "append it with a deadline or time period."
+			+ NEWLINE + "Formats supported:"
+			+ NEWLINE + "add [task]"
+			+ NEWLINE + "add [task] by [end_time]" 
+			+ NEWLINE + "add [task] from [start_time] to [end_time]";
 	
 	public static final String VIEW_HELP_MESSAGE_BRIEF = "Displays the upcoming task or the tasks for the day." + "\n"
 			+ "A time frame can be specified to manage your schedule better within that period.";
 	
-	public static final String VIEW_HELP_MESSAGE_DETAILED = "Displays the upcoming task or the tasks for the day." + "\n"
-			+ "A time frame can be specified to manage your schedule better within that period."  + "\n"
-			+ "Formats supported:" + "\n"
-			+ "	view all" + "\n"
-			+ "	view next" + "\n"
-			+ "	view [day|date]" + "\n"
-			+ "	view from [start_time] to [end_time]";
+	public static final String VIEW_HELP_MESSAGE_DETAILED = "view" + INDENT + "Display the upcoming task or the tasks"
+			+ NEWLINE + INDENT + "for the day. A time frame can be" 
+			+ NEWLINE + INDENT + "specified too."
+			+ NEWLINE + "Formats supported:"
+			+ NEWLINE + "view all"
+			+ NEWLINE + "view next"
+			+ NEWLINE + "view [day|date]"
+			+ NEWLINE + "view from [start_time] to [end_time]";
 	
 	public static final String UPDATE_HELP_MESSAGE_BRIEF = "Update the description, deadline, start, or end time of a task.";
 	
-	public static final String UPDATE_HELP_MESSAGE_DETAILED =  "Update the description, deadline, start, or end time of a task." + "\n"
-			+ "Formats supported:" + "\n"
-			+ "	update [task_id] [new_description]" + "\n"
-			+ "	update [task_id] by [end_time]" + "\n"
-			+ "	update [task_id] from [start_time] to [end_time]";
+	public static final String UPDATE_HELP_MESSAGE_DETAILED =  "update" + INDENT + "Update the description, deadline, start"
+			+ NEWLINE + INDENT + "or end time of a task."
+			+ NEWLINE + "Formats supported:"
+			+ NEWLINE + "update [task_id] [new_description]"
+			+ NEWLINE + "update [task_id] by [end_time]"
+			+ NEWLINE + "update [task_id] from [start_time] to [end_time]";
 	
 	public static final String DELETE_HELP_MESSAGE_BRIEF = "Delete by id, date, deadline, or time frame.";
 	
-	public static final String DELETE_HELP_MESSAGE_DETAILED = "Delete by id, date, deadline, or time frame." + "\n"
-			+ "Formats supported:" + "\n"
-			+ "	delete [task_id]" + "\n"
-			+ "	delete deadline" + "\n"
-			+ "	delete [date]" + "\n"
-			+ "	delete from [start_time] to [end_time]";
+	public static final String DELETE_HELP_MESSAGE_DETAILED = "Delete by id, date, deadline, or time frame."
+			+ NEWLINE + "Formats supported:"
+			+ NEWLINE + "delete [task_id]"
+			+ NEWLINE + "delete deadline"
+			+ NEWLINE + "delete [date]"
+			+ NEWLINE + "delete from [start_time] to [end_time]";
 	
 	public static final String DONE_HELP_MESSAGE_BRIEF = "Label a task as done.";
 	
-	public static final String DONE_HELP_MESSAGE_DETAILED = "Label a task as done." + "\n"
-			+ "Formats supported:" + "\n"
-			+ " done [task_id]";
+	public static final String DONE_HELP_MESSAGE_DETAILED = "Label a task as done."
+			+ NEWLINE + "Formats supported:"
+			+ NEWLINE + "done [task_id]";
 	
-	public static final String SEARCH_HELP_MESSAGE = "Formats supported:" + "\n" + "	search [keywords]";
+	public static final String SEARCH_HELP_MESSAGE = "search" + INDENT + "Search for a task by its keyword."
+	+ NEWLINE + "Formats supported:" 
+	+ NEWLINE + "search [keywords]";
 	
-	public static final String UNDO_HELP_MESSAGE = "Undo works for a maximum of two times.";
+	public static final String UNDO_HELP_MESSAGE = "undo" + INDENT + "Undo most recent add/update/delete.";
 	
-	public static final String REDO_HELP_MESSAGE = "Redo works for a maximum of " + "\n"
-			+ "the number of consecutive undo commands, ";
+	public static final String REDO_HELP_MESSAGE = "redo" + INDENT + "Redo the most recent undo.";
 	
-	public static final String EXIT_HELP_MESSAGE = "Exit WhatsUpNext.";
+	public static final String EXIT_HELP_MESSAGE = "exit" + INDENT + "Close WhatsUpNext.";
 	
 }
